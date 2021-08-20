1c8f9337cb982fd7533ba26a3fda1023e8f7560f
Lauriethefish <lauriethefish@outlook.com>
2021-08-20 13:59:50 +0100

    Fix cover image

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -10,5 +10,5 @@ $ArchiveName = "mod-list_v0.1.2.qmod"
 $TempArchiveName = "mod-list_v0.1.2.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libmod-list.so", "./libs/arm64-v8a/libbeatsaber-hook_2_2_5.so", "./mod.json" -DestinationPath $TempArchiveName -Force
+Compress-Archive -Path "./libs/arm64-v8a/libmod-list.so", "./libs/arm64-v8a/libbeatsaber-hook_2_2_5.so", "cover.png", "./mod.json" -DestinationPath $TempArchiveName -Force
 Move-Item $TempArchiveName $ArchiveName -Force
