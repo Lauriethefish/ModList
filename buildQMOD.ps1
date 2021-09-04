68425a6764b2125e2f344504cbd1f45c0ffb4df4
Lauriethefish <lauriethefish@outlook.com>
2021-09-04 06:37:43 +0100

    Update to BS v1.17.0

--- buildQMOD.ps1
+++ buildQMOD.ps1
@@ -10,5 +10,5 @@ $ArchiveName = "mod-list_v0.1.3.qmod"
 $TempArchiveName = "mod-list_v0.1.3.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libmod-list.so", "./libs/arm64-v8a/libbeatsaber-hook_2_2_5.so", "cover.png", "./mod.json" -DestinationPath $TempArchiveName -Force
+Compress-Archive -Path "./libs/arm64-v8a/libmod-list.so", "./libs/arm64-v8a/libbeatsaber-hook_2_3_0.so", "cover.png", "./mod.json" -DestinationPath $TempArchiveName -Force
 Move-Item $TempArchiveName $ArchiveName -Force
